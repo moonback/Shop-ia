abe1174b3b4a95519a7f196f0efcd2b965c1aa0a
Medusa <devoye.maysson@gmail.com>
2026-03-08 21:01:24 +0100

    up

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,22 @@
-const CACHE_NAME = 'green-moon-v1';
+const CACHE_NAME = 'green-moon-v2';
 const STATIC_ASSETS = [
     '/',
     '/index.html',
-    '/logo.png',
-    '/logo.jpeg',
+    '/favicon.ico',
+    '/favicon-16x16.png',
+    '/favicon-32x32.png',
+    '/favicon-48x48.png',
+    '/icon-72x72.png',
+    '/icon-96x96.png',
+    '/icon-128x128.png',
+    '/icon-144x144.png',
+    '/icon-152x152.png',
+    '/icon-167x167.png',
+    '/apple-touch-icon.png',
+    '/icon-192x192.png',
+    '/icon-384x384.png',
+    '/icon-512x512.png',
+    '/manifest.webmanifest',
     '/splash.mp4',
 ];
 
